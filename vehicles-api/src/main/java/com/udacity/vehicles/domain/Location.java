52b80c3a9c2d007c332450170f1cc0d9328d74c9
Pydare <dareyadewumi650@gmail.com>
2020-09-28 15:34:19 +0100

    first commit

---
+++ vehicles-api/src/main/java/com/udacity/vehicles/domain/Location.java
@@ -0,0 +1,93 @@
+package com.udacity.vehicles.domain;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Stores information about a given location.
+ * Latitude and longitude must be provided, while other
+ * location information must be gathered each time from
+ * the maps API.
+ */
+
+//location has lat and lon which is used to get more address details from the maps client microservice
+//this whole address is set in the address field of this class. Location is a field in car class, it is then
+//finally set there.
+@Embeddable
+public class Location {
+
+    @NotNull
+    private Double lat;
+
+    @NotNull
+    private Double lon;
+
+    @Transient
+    private String address;
+
+    @Transient
+    private String city;
+
+    @Transient
+    private String state;
+
+    @Transient
+    private String zip;
+
+    public Location() {
+    }
+
+    public Location(Double lat, Double lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+}
